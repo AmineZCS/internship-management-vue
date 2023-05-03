1f62522b10795838477722d07c747d72b8b704bc
AmineZCS <medamineben19@gmail.com>
2023-05-03 18:09:05 +0100

    added user and internship store in vux

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,48 @@
 import { createStore } from 'vuex'
-
-export default createStore({
+// user data store
+const user = {
   state: {
+    user: null
   },
   getters: {
+    user: state => state.user
   },
   mutations: {
+    setUser(state, user) {
+      state.user = user
+    }
   },
   actions: {
+    setUser({ commit }, user) {
+      commit('setUser', user) 
+    }
+  }
+}
+
+const selectedInternship = {
+  state: {
+    selectedInternship: null
   },
+  getters: {
+    selectedInternship: state => state.selectedInternship
+  },
+  mutations: {
+    setSelectedInternship(state, selectedInternship) {
+      state.selectedInternship = selectedInternship
+    }
+  },
+  actions: {
+    setSelectedInternship({ commit }, selectedInternship) {
+      commit('setSelectedInternship', selectedInternship)
+    }
+  }
+}
+
+
+
+export default createStore({
   modules: {
+    user,
+    selectedInternship
   }
 })
