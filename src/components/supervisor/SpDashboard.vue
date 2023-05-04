d456a2a0643fc6e13c0f9a6c0e71aac93c45746e
AmineZCS <medamineben19@gmail.com>
2023-05-04 15:29:01 +0100

    supervisor components

---
+++ src/components/supervisor/SpDashboard.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-app >
+    
+    <SpSidebar/>
+    
+    <v-content>
+      
+      <router-view/>
+    </v-content>
+  </v-app>
+</template>
+<script>
+import { mapActions } from 'vuex'
+import SpSidebar from './SpSidebar.vue';
+export default {
+  name: 'SpDashboard',
+  components: {
+    SpSidebar,
+ 
+  },
+  methods: {
+    apply() {
+      // handle apply button click
+    },
+    ...mapActions('user', ['checkAuthentication']),
+  },
+}
+</script>
+<style >
+body {
+  padding-left: 78px;
+}
+</style>
---
+++ src/components/supervisor/SpSidebar.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app-bar app>
+    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-toolbar-title>My App</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <!-- Notifications component -->
+    <notificationsComp/>
+  </v-app-bar>
+  <v-navigation-drawer v-model="drawer" app
+      >
+        <v-list
+          density="compact"
+          nav
+        >
+        <v-list-item prepend-icon="mdi-view-dashboard" title="Home" value="home"></v-list-item>
+          
+          <v-list-item prepend-icon="mdi-account" title="Profile" value="Profile"></v-list-item>
+          <v-list-item prepend-icon="mdi-logout" title="Logout" value="Logout" @click="logout"></v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+</template>
+
+<script>
+import { mapActions } from 'vuex'
+import notificationsComp from '../notificationsComp.vue';
+export default {
+  data() {
+    return {
+      drawer: true,
+    };
+  },
+  components: {
+    notificationsComp,
+  },
+  methods: {
+    ...mapActions('user', ['logout']),
+  },
+  
+
+};
+</script>
+
+
+
+<style scoped>
+v-list-item__content {
+  display: flex;
+  flex-direction: row;
+}
+</style>
