65737d94b83d878481f4eb8dc602e5aaba014984
Zakaria Nasri <129872644+ZakariaNasri@users.noreply.github.com>
2023-06-03 21:27:24 +0100

    signUp

---
+++ src/components/SignUpAmine.vue
@@ -0,0 +1,358 @@
+<template>
+    <div class="Holder">
+      <v-row class="my-0">
+        <v-col cols="12" md="5" class="d-flex left-col pb-0" style="position: relative;">
+            <img class="logo" src="../../../../forAmine/used/PNG/IAWhite.png" alt="">
+            <p class="logoName">InternshipAxis.</p>
+            <div class="left-text-holder">
+                <h1 class="left-header mb-5">Let's get started .</h1>
+                <h1 class="left-header mb-5">Sign up as a supervisor </h1>
+                <p style="color: white; font-weight: 300;">Join us today and make a difference in the lives of interns <br> 
+                    as they embark on their professional journeys. <br> Together, we can create a 
+                    supportive and enriching <br> environment for the leaders of tomorrow.</p>
+                <v-btn class="gobackBtn" rounded color="white" @click="goBack">
+                    <v-icon>mdi-arrow-left</v-icon>
+                      go back
+                </v-btn>
+            </div>
+
+        </v-col>
+
+
+        <v-col style="height: 100vh !important;" cols="12" md="7" class="d-flex justify-center align-center pb-0">
+          <v-stepper v-model="e1" style="width: 60% !important; height: 80%; border-radius: 20px;">
+            <v-stepper-header>
+              <template v-for="n in steps">
+                <v-stepper-step
+                  color="#f7ab3e"
+                  :key="`${n}-step`"
+                  :complete="e1 > n"
+                  :step="n"
+                  editable
+                >
+                  Step {{ n }}
+                </v-stepper-step>
+  
+                <v-divider
+                  v-if="n !== steps"
+                  :key="n"
+                ></v-divider>
+              </template>
+            </v-stepper-header>
+  
+            <v-stepper-items>
+              <v-stepper-content
+                v-for="n in steps"
+                :key="`${n}-content`"
+                :step="n"
+              >
+
+                <!-- Presonal Infoo -->
+
+                <v-card
+                  class="mb-12 text-center"
+                  height="390px"
+                  elevation="0"
+                  v-if="n===1"
+                  style="height: 100% !important;"
+                >
+                <h1 class="headings">Personal Inforamations</h1>
+                
+                <v-row>
+                    <v-col cols="12" md="12">
+                        <v-file-input
+                            v-model="supervisor.image"
+                            color="#f7ab3e" 
+                            label="Upload Your Image"
+                            id="id"
+                        ></v-file-input>
+                    </v-col>
+                </v-row>
+                
+                <v-row>
+                    <v-col cols="12" md="6">
+                        <v-text-field
+                            v-model="supervisor.fname" 
+                            color="#f7ab3e"
+                            label="First Name"
+                            id="id"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="6">
+                        <v-text-field
+                            v-model="supervisor.lname" 
+                            color="#f7ab3e"
+                            label="Last Name"
+                            id="id"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="12" md="6">
+                        <v-text-field
+                            v-model="supervisor.phoneNumber" 
+                            color="#f7ab3e"
+                            label="Phone Number"
+                            id="id"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="6">
+                        <v-text-field
+                            v-model="supervisor.bio" 
+                            color="#f7ab3e"
+                            label="Biography"
+                            id="id"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="12" md="6">
+                        <v-text-field
+                            v-model="supervisor.email"
+                            color="#f7ab3e"
+                            label="Email"
+                            id="id"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="6">
+                        <v-text-field
+                            v-model="supervisor.password"
+                            color="#f7ab3e"
+                            label="Paasword"
+                            id="id"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+            </v-card>
+
+            <!-- Comani Infoo -->
+            <v-card
+                  class="mb-12 text-center"
+                  height="390px"
+                  elevation="0"
+                  v-if="n===2"
+                >
+                <h1 class="headings">Company Inforamations</h1>
+                <v-row>
+                    <v-col cols="12" md="12">
+                        <v-file-input
+                            v-model="company.image"
+                            color="#f7ab3e"
+                            label="Company Image"
+                            id="id"
+                        ></v-file-input>
+                    </v-col>
+                </v-row>
+                <v-row>
+                    <v-col cols="12" md="6">
+                        <v-text-field
+                            v-model="company.name"
+                            color="#f7ab3e"
+                            label="Name"
+                            id="id"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="6">
+                        <v-text-field
+                            v-model="company.email"
+                            color="#f7ab3e"
+                            label="Email"
+                            id="id"
+                        ></v-text-field>
+                    </v-col>
+                    
+                </v-row>
+
+                <v-row>
+                    <v-col cols="12" md="6">
+                        <v-text-field
+                            v-model="company.phoneNumber"
+                            color="#f7ab3e"
+                            label="Phone Number"
+                            id="id"
+                        ></v-text-field>
+                    </v-col>
+                    <v-col cols="12" md="6">
+                        <v-text-field
+                            v-model="company.address"
+                            color="#f7ab3e"
+                            label="Address"
+                            id="id"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+
+                <v-row>
+                    <v-col cols="12" md="12">
+                        <v-text-field
+                            v-model="company.bio"
+                            color="#f7ab3e"
+                            label="Biographie"
+                            id="id"
+                        ></v-text-field>
+                    </v-col>
+                </v-row>
+
+            </v-card>
+            <div style="float: right !important; margin-bottom: 5px;">
+                <v-btn v-if="n===1" class="ml-2" rounded text>
+                  Cancel
+                </v-btn>
+
+                <v-btn
+                v-if="n===1"
+                  color="#f7ab3e"
+                  rounded
+                  @click="nextStep(n)"
+                >
+                  continue
+                </v-btn>
+            </div>
+                
+            <div style="float: right !important; margin-bottom: 5px;">
+                <v-btn v-if="n===2" class="ml-2" rounded text>
+                  Cancel
+                </v-btn>
+                <v-btn
+                v-if="n===2"
+                  color="#f7ab3e"
+                  rounded
+                  @click="signUp"
+                >
+                  Sign up
+                </v-btn>
+            </div>
+              </v-stepper-content>
+            </v-stepper-items>
+          </v-stepper>
+        </v-col>
+      </v-row>
+    </div>
+  </template>
+  
+
+<script>
+import api from '@/api'
+export default {
+  data () {
+    return {
+      e1: 1,
+      steps: 2,
+      supervisor:{
+        fname:'',
+        lname:'',
+        email:'',
+        password:'',
+        phoneNumber:'',
+        bio:'',
+        image:''
+      },
+      company:{
+        name:'',
+        address:'',
+        email:'',
+        phoneNumber:'',
+        bio:'',
+        image:''
+      }
+    }
+  },
+
+  watch: {
+    steps (val) {
+      if (this.e1 > val) {
+        this.e1 = val
+      }
+    },
+  },
+
+  methods: {
+    nextStep (n) {
+      if (n === this.steps) {
+        this.e1 = 1
+      } else {
+        this.e1 = n + 1
+      }
+    },
+    async signUp(){
+        try{
+            await api.post('/signUpSupervisor',{
+                fname:this.supervisor.fname,
+                lname:this.supervisor.lname,
+                email:this.supervisor.email,
+                password:this.supervisor.password,
+                user_phone_number:this.supervisor.phoneNumber,
+                user_bio:this.supervisor.bio,
+                user_image:this.supervisor.image,
+                company_name:this.company.name,
+                company_email:this.company.email,
+                company_phone_number:this.company.phoneNumber,
+                company_address:this.company.address,
+                company_bio:this.company.bio,
+                company_image:this.company.image,
+            })
+            .then(response => {
+              console.log(response)
+            })
+        }catch(error) {
+            console.log(error);
+          }
+    },
+    goBack(){
+        this.$router.push('/')
+    }
+  },
+}
+</script>
+
+<style scoped>
+    .Holder {
+  width: 100%;
+  height: 100vh;
+}
+    .headings{
+        font-family: Outfit;
+        color: #3a3849;
+    }
+    .colors{
+    color: #f7ab3e;
+    color: #3a3849;
+  }
+  .left-header{
+    color: white;
+    font-family: Outfit;
+    font-size: 60px;
+  }
+  .left-text-holder{
+    margin-top:100px;
+    margin-left: 60px;
+  }
+  .left-col{
+background-image: url(../../../../forAmine/used/PNG/left.png);
+  background-size: cover;
+  background-repeat: no-repeat;
+  background-position: center; 
+  }
+  .logo{
+    width: 60px;
+    height: 50px;
+    position: absolute;
+    top: 10px;
+    left: 20px;
+  }
+  .logoName{
+    position: absolute;
+    font-family: outfit;
+    font-size: 20px;
+    color: white;
+    font-weight: 500;
+    top: 15px;
+    right: 20px;
+  }
+  .gobackBtn{
+    position: absolute;
+    bottom: 30px;
+  }
+</style>
